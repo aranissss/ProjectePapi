a78ec15b6094a15328789e1a327b43abeba200cb
Aran <128226788+aranissss@users.noreply.github.com>
2025-06-29 14:01:59 +0200

    Update chat_openai.py

--- llm/chat_openai.py
+++ llm/chat_openai.py
@@ -7,14 +7,14 @@ import streamlit as st
 if "OPENAI_API_KEY" not in st.secrets:
     st.warning("⚠️ OPENAI_API_KEY not found in secrets!")
 else:
-   st.write("Loaded keys:", st.secrets['OPENAI_API_KEY'])
+   st.write("Loaded keys inside llm:", st.secrets['OPENAI_API_KEY'])
 class LLMClient:
     def __init__(self, stream: bool = False):
         st.write("🔐 Secrets loaded:", list(st.secrets.keys()))
         print(st.secrets.keys())
         self.__llm = ChatOpenAI(
             model="gpt-3.5-turbo",
-            api_key=st.secrets['OPENAI_API_KEY'],
+            api_key= st.secrets['OPENAI_API_KEY']],
             #api_key=os.getenv('OPENAI_API_KEY'),
             #response_format="json",  # ← actual recomendado
         )
